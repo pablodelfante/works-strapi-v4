592525e1787067fc7339a1c1eaa4157ca39a7f21
pablo <pablodelfantexp@gmail.com>
2021-12-29 15:50:40 -0300

    chore: add basic credential as anonymus enviroment

--- config/database.js
+++ config/database.js
@@ -2,10 +2,18 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'ec2-34-230-133-163.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'dakjbfj8kn1pui'),
+      user: env('DATABASE_USERNAME', 'grfisgqvccylra'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
