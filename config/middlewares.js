9be15ebbf3f7d767495e0012a1787a73cf842897
pablo <pablodelfantexp@gmail.com>
2022-01-16 20:29:40 -0300

    refactor: fix the image preview on the media library

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,22 @@
-module.exports = [
+/***
+ * Solución para el error de miniatura de imagen
+ * https://forum.strapi.io/t/strapi-v4-broken-image-even-when-the-images-link-works-cloudinary/14143/3
+ * https://forum.strapi.io/t/strapi-v4-broken-image-even-when-the-images-link-works-cloudinary/14143/3
+*/
+
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `https://res.cloudinary.com/`],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
