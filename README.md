56c55acc0bdbffc4b9567dd0af78cc64b1187761
pablo <pablodelfantexp@gmail.com>
2022-01-17 15:58:42 -0300

    chore: add works example to explore new version

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Strapi application
 
-A quick description of your strapi application
+### To start services
+    docker-compose up
--- src/api/work/controllers/work.js
+++ src/api/work/controllers/work.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  test controller
+ *  work controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::test.test');
+module.exports = createCoreController('api::work.work');
